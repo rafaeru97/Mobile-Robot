fe23620b79ec4d7b39a59b6933cff19115f51bff
rafaeru97 <sqadiam@gmail.com>
2024-08-26 16:03:44 +0200

    init files

---
+++ robot/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+from .motor_controller import MotorController
+from .encoder import Encoder
+from .gyro import Gyro
+from .distance_sensor import DistanceSensor
+from .mapper import Mapper
+from .utils import calculate_new_position, angle_to_radians
---
+++ robot/distance_sensor.py
@@ -0,0 +1,9 @@
+class DistanceSensor:
+    def __init__(self, trigger_pin, echo_pin):
+        self.trigger_pin = trigger_pin
+        self.echo_pin = echo_pin
+        # Setup GPIO pins here
+
+    def get_distance(self):
+        # Measure and return distance
+        pass

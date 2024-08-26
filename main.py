08988b4cc9bfc0a80afca814f77322c4d79ac031
rafaeru97 <sqadiam@gmail.com>
2024-08-26 20:22:02 +0200

    control interface

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main(stdscr):
     right_encoder = Encoder(pin_a=16, pin_b=1, wheel_diameter=0.08, ticks_per_revolution=960)
     gyro = Gyro()  # Inicjalizuj swój żyroskop
 
+    # Flaga do zarządzania operacjami
+    is_busy = False
+
     # Włącz tryb nie-blokujący w curses
     stdscr.nodelay(1)
     stdscr.timeout(100)
@@ -17,18 +20,26 @@ def main(stdscr):
         while True:
             key = stdscr.getch()
 
-            if key == curses.KEY_UP:
+            if key == curses.KEY_UP and not is_busy:
                 stdscr.addstr(0, 0, 'Moving Forward')
+                is_busy = True
                 motor_controller.forward_with_encoders(left_encoder, right_encoder, 0.1)
-            elif key == curses.KEY_DOWN:
+                is_busy = False
+            elif key == curses.KEY_DOWN and not is_busy:
                 stdscr.addstr(0, 0, 'Moving Backward')
+                is_busy = True
                 motor_controller.backward_with_encoders(left_encoder, right_encoder, 0.1)
-            elif key == curses.KEY_LEFT:
+                is_busy = False
+            elif key == curses.KEY_LEFT and not is_busy:
                 stdscr.addstr(0, 0, 'Rotating Left')
-                motor_controller.rotate_to_angle(gyro, target_angle=9, direction='left', speed=50)
-            elif key == curses.KEY_RIGHT:
+                is_busy = True
+                motor_controller.rotate_to_angle(gyro, target_angle=90, direction='left', speed=50)
+                is_busy = False
+            elif key == curses.KEY_RIGHT and not is_busy:
                 stdscr.addstr(0, 0, 'Rotating Right')
-                motor_controller.rotate_to_angle(gyro, target_angle=9, direction='right', speed=50)
+                is_busy = True
+                motor_controller.rotate_to_angle(gyro, target_angle=90, direction='right', speed=50)
+                is_busy = False
             elif key == ord('q'):
                 stdscr.addstr(0, 0, 'Quitting')
                 break

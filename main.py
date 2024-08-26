60b36fdc767b56a2d8d09d22303606cf7f2ea797
rafaeru97 <sqadiam@gmail.com>
2024-08-26 18:22:51 +0200

    encoders fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 def main():
     # Tworzenie obiektów enkoderów
     left_encoder = Encoder(pin_a=19, pin_b=26, wheel_diameter=0.1, ticks_per_revolution=360)
-    right_encoder = Encoder(pin_a=1, pin_b=16, wheel_diameter=0.1, ticks_per_revolution=360)
+    right_encoder = Encoder(pin_a=16, pin_b=1, wheel_diameter=0.1, ticks_per_revolution=360)
 
     try:
         while True:

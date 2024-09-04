69eb5a3a9737bd572e9c387eff07fd4239cd79e2
rafaeru97 <sqadiam@gmail.com>
2024-09-04 20:28:01 +0200

    fix pid to rotate movement

--- robot/motor_controller.py
+++ robot/motor_controller.py
@@ -225,7 +225,7 @@ class MotorController:
 
                 # Obliczaj prędkość obrotu na podstawie PID
                 control = pid.compute(current_angle, dt)
-                control = max(0, min(100, control))
+                control = max(30, min(100, control))
                 print(f"control: {control}")
 
                 if direction == 'left':

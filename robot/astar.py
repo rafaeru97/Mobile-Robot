69b3b813356505c073fd7582650bd36bda14e0d8
rafaeru97 <sqadiam@gmail.com>
2024-09-10 21:52:12 +0200

    mapper fix

--- robot/astar.py
+++ robot/astar.py
@@ -79,8 +79,8 @@ class AStarPathfinder:
             robot_x, robot_y = robot_position
 
             # Dodanie offsetu - odejmowanie współrzędnych robota pomniejszonych o 100
-            path[:, 0] -= robot_y
-            path[:, 1] -= robot_x
+            path[:, 0] -= robot_x
+            path[:, 1] -= robot_y
 
             plt.plot(path[:, 1], path[:, 0], 'r-', lw=2, label='Path')
 

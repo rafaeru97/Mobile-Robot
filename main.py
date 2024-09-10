75bb2225b508c2d8da4b9c988bfd6e253e1e2840
rafaeru97 <sqadiam@gmail.com>
2024-09-10 17:03:19 +0200

    mapper filters fix

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def main(stdscr):
         right_encoder = Encoder(pin_a=16, pin_b=1, wheel_diameter=0.08, ticks_per_revolution=960)
         motor_controller = MotorController()
         motor_controller.setEncoders(left_encoder, right_encoder)
-        gyro = Gyro()
+        gyro = Gyro(calib_value=-250)
         sensor = DistanceSensor(trigger_pin=23, echo_pin=24)
         mapper = Mapper(motor_controller, gyro, sensor)
         mapper.create_map()

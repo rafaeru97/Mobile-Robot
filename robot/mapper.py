d83575963edef1807866143957d66a68b1742b3d
rafaeru97 <sqadiam@gmail.com>
2024-09-10 17:13:12 +0200

    mapper filters fix

--- robot/mapper.py
+++ robot/mapper.py
@@ -217,7 +217,7 @@ class Mapper:
         dist_matrix = distance_matrix(points, points)
 
         # Threshold to consider a point as a neighbor
-        threshold = 4  # Adjust this value based on your data
+        threshold = 1  # Adjust this value based on your data
 
         # Filter points that are isolated
         filtered_points = []

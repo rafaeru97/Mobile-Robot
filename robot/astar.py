21b7da63a3e39f7c489bdcdaa86e483a2ac678ca
rafaeru97 <sqadiam@gmail.com>
2024-09-11 16:36:57 +0200

    pathfinding

--- robot/astar.py
+++ robot/astar.py
@@ -141,7 +141,7 @@ class AStarPathfinder:
         plt.figure(figsize=(8, 8))
 
         # Wyświetlenie mapy siatki
-        plt.imshow(map_grid, cmap='gray', origin='upper')
+        plt.imshow(map_grid, cmap='gray', origin='lower')
         plt.gca().invert_yaxis()
         # Rozpoczęcie i zakończenie ścieżki
         if path:

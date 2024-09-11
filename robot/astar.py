c7d5d11cb3d8db0ccab2d928df3a5216396e8962
rafaeru97 <sqadiam@gmail.com>
2024-09-11 19:32:45 +0200

    pathfinding

--- robot/astar.py
+++ robot/astar.py
@@ -99,9 +99,8 @@ class AStarPathfinder:
         """Oblicza odległość Manhattan pomiędzy dwoma punktami na siatce."""
         return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-    def astar(self, start, goal):
+    def astar(self, start, goal, output_file='path.txt'):
         """Główna funkcja A* z uwzględnieniem odbicia Y i offsetu."""
-        # Przesuwanie współrzędnych startowych i docelowych według offsetu i skali
         start_grid = self.world_to_grid(start)
         goal_grid = self.world_to_grid(goal)
 
@@ -117,7 +116,14 @@ class AStarPathfinder:
             current = heapq.heappop(open_list)[1]
 
             if current == goal_grid:
-                return self.reconstruct_path(came_from, current)
+                path = self.reconstruct_path(came_from, current)
+
+                # Zapisanie ścieżki do pliku
+                with open(output_file, 'w') as f:
+                    for point in path:
+                        f.write(f"{point[0]}, {point[1]}\n")
+
+                return path
 
             for neighbor in self.get_neighbors(current):
                 tentative_g_score = g_score[current] + self.distance(current, neighbor)

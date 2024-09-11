37ed2b5fc75e8438e927b5e4d85143398f41710e
rafaeru97 <sqadiam@gmail.com>
2024-09-11 18:26:00 +0200

    pathfinding

--- robot/mapper.py
+++ robot/mapper.py
@@ -234,7 +234,7 @@ class Mapper:
         plt.savefig(filename)
         plt.show()
 
-    def generate_map_grid_for_astar(self, resolution=1.0, dilation_radius=2, erosion_radius=2, min_width=200,
+    def generate_map_grid_for_astar(self, resolution=1.0, dilation_radius=2, erosion_radius=1, min_width=200,
                                     min_height=200,
                                     center_x=100, center_y=100):
         """

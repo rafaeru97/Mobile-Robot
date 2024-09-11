a322cbc51b4d9e9a221d2894c9f1f503977d3672
rafaeru97 <sqadiam@gmail.com>
2024-09-11 18:01:43 +0200

    pathfinding

--- robot/astar.py
+++ robot/astar.py
@@ -113,8 +113,8 @@ class AStarPathfinder:
     def get_neighbors(self, node):
         x, y = node
         neighbors = []
-        robot_size = 6  # Rozmiar robota w cm
-        buffer = robot_size // 2
+        robot_size = 1  # Rozmiar robota w cm
+        buffer = robot_size // 1
 
         for i in range(-buffer, buffer + 1):
             for j in range(-buffer, buffer + 1):
@@ -129,8 +129,8 @@ class AStarPathfinder:
 
     def is_valid(self, node):
         x, y = node
-        robot_size = 6  # Rozmiar robota w cm
-        buffer = robot_size // 2  # Bufor wokół robota, aby upewnić się, że jest wystarczająco dużo miejsca
+        robot_size = 1  # Rozmiar robota w cm
+        buffer = robot_size // 1  # Bufor wokół robota, aby upewnić się, że jest wystarczająco dużo miejsca
 
         # Sprawdź, czy wszystkie punkty w obszarze robota są wolne
         for i in range(-buffer, buffer + 1):

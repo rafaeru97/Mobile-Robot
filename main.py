570c0e39b27f1eee7c6e2823d5906e7ef2e69643
rafaeru97 <sqadiam@gmail.com>
2024-09-12 17:49:22 +0200

    fixes

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def print_gui_data(stdscr, speed, distance, orientation, rotate, motor_status, e
         robot_pos = mapper.get_pos()
         robot_grid_pos = mapper.get_grid_pos()
 
-        stdscr.addstr(height // 2 + 7, 2, f"Robot Position:  ({mapper.robot_pos[0]:.2f}, {robot_pos[1]:.2f})")
+        stdscr.addstr(height // 2 + 7, 2, f"Robot Position:  ({robot_pos[0]:.2f}, {robot_pos[1]:.2f})")
         stdscr.addstr(height // 2 + 8, 2, f"Grid Position:  ({int(robot_grid_pos[0])}, {int(robot_grid_pos[1])})")
 
         stdscr.addstr(height - 1, 2, "Press Ctrl+C to exit.")
